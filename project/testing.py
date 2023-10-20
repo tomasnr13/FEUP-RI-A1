2c8934e4390cd49a4dc76a8bd004ff838eaa654e
PPinhas <pedro.s.pinheiro2001@gmail.com>
2023-10-20 22:40:01 +0100

    random position start

--- project/testing.py
+++ project/testing.py
@@ -1,47 +1,34 @@
 import subprocess
+import random
+import yaml
+import math
 
-constants_code_template = """
-# Constants
-MAX_LIN_VEL = {}
-MAX_ANG_VEL = {}
-LIN_ACC = {}
-LIN_DEC = {}
-ANG_ACC = {}
-ANG_DEC = {}
-MIN_DIST_FROM_WALL = {}
-LASER_RANGE = {}
-LASER_FREQ = {}
-STOP_WALL_LEN = {}
-STOP_TOLERANCE = {}
-FILENAME = {}
-"""
+def random_point_in_circle(center, radius):
+    # Generate random angle and radius within the circle
+    angle = random.uniform(0, 2 * math.pi)
+    r = math.sqrt(random.uniform(0, 1)) * radius
 
-def write_constants_and_execute():
+    # Convert to Cartesian coordinates
+    x = center[0] + r * math.cos(angle)
+    y = center[1] + r * math.sin(angle)
 
-    counter = 0
+    return [x, y]
 
-    MAX_LIN_VEL = 2.0
-    MAX_ANG_VEL = 3.0
-    LIN_ACC = 1.5
-    LIN_DEC = 3.0
-    ANG_ACC = 6.0
-    ANG_DEC = 6.0
-    MIN_DIST_FROM_WALL = 1.0
-    LASER_RANGE = 5
-    LASER_FREQ = 10
-    STOP_WALL_LEN = 4.18
-    STOP_TOLERANCE = 0.1
+def modify_yaml_and_execute():
+    with open('./src//world/turtle.model.yaml', 'r') as file:
+        yaml_data = yaml.safe_load(file)
 
-    for i in range(5):
-        counter += 1
-        FILENAME = f"\'odometry_{MAX_LIN_VEL}_{MAX_ANG_VEL}_{LIN_ACC}_{LIN_DEC}_{ANG_ACC}_{ANG_DEC}_{MIN_DIST_FROM_WALL}_{LASER_RANGE}_{LASER_FREQ}_{STOP_WALL_LEN}_{STOP_TOLERANCE}.csv\'"
-        constants_code = constants_code_template.format(MAX_LIN_VEL, MAX_ANG_VEL, LIN_ACC, LIN_DEC, ANG_ACC, ANG_DEC, MIN_DIST_FROM_WALL, LASER_RANGE, LASER_FREQ, STOP_WALL_LEN, STOP_TOLERANCE, FILENAME)
+    circle = random_point_in_circle([3,0], 3)
 
-        with open('./src/turtle/params.py', 'w') as f:
-            f.write(constants_code)
+    yaml_data['bodies'][0]['pose'][0] = circle[0] #center 3, min -1 max 7
+    yaml_data['bodies'][0]['pose'][1] = circle[1] #center 0 min -4 max 4
 
-        subprocess.run(['colcon', 'build'])
-        subprocess.run(['source', 'install/setup.bash'], shell=True)
-        subprocess.run(['ros2', 'launch', 'turtle', 'turtle.launch.py'])
+    with open('./src//world/turtle.model.yaml', 'w') as file:
+        yaml.dump(yaml_data, file)
 
-write_constants_and_execute()
+    subprocess.run(['colcon', 'build'])
+    subprocess.run(['source', 'install/setup.bash'], shell=True)
+    subprocess.run(['ros2', 'launch', 'turtle', 'turtle.launch.py'])
+
+if __name__ == "__main__":
+    modify_yaml_and_execute()
